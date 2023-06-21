fbd4bd1575b39ec8ae7ccdb392bf4e5a63f13964
Yuvraj Bhonsle <yuvrajyhb@gmail.com>
2023-06-22 00:58:06 +0530

    changes made in about and glossary page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -8,14 +8,15 @@ const About = () => {
   const [expandedItem, setExpandedItem] = useState(null);
 
   return (
-    <section className="flex flex-col justify-evenly items-center w-[80%] mx-auto my-3 border border-black h-[100vh] gap-4 lg:flex-row">
-      <div className="w-[50%] h-[20rem] rounded-full object-fill border border-black">
+    <section className="flex flex-col justify-evenly items-center w-[80%] mx-auto my-3 h-[100vh] gap-4 lg:flex-row">
+      {/* <div className="w-[50%] h-[20rem] rounded-full object-fill border border-black">
         <img src="../../public/logo-2.png" alt="about" />
       </div>
       <div>
         <h1>Nishi Motta</h1>
         <p>Masters, Studying in MSU, Created Greek God Calculator</p>
-      </div>
+      </div> */}
+      <h1 className="text-2xl">Hi, I'm Nishi Motta</h1>
     </section>
   );
 };
