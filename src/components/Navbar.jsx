55444150371d6089e5f7e92caa6cd13251863c15
Yuvraj Bhonsle <yuvrajyhb@gmail.com>
2023-06-23 21:40:01 +0530

    fixed: navbar close on clicking new link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
       <div className="flex justify-between xl:max-w-7xl xl:mx-auto max-w-full px-[8%] p-1 items-center flex-wrap w-full">
         <div className="w-[4rem] cursor-pointer rounded-full text-4xl">
           <Link to="/">
-            <img src="/logo.png" alt="logo" className="rounded-full text-4xl"/>
+            <img src="/logo.png" alt="logo" className="rounded-full text-4xl" />
           </Link>
         </div>
 
@@ -35,14 +35,27 @@ const Navbar = () => {
         >
           <ul className="text-lg text-gray-600 lg:flex lg:justify-between">
             <li className="lg:px-5 py-2 block text-center hover:text-yellow-700 font-semibold">
-              <NavLink to="/" className="pb-1">Home</NavLink>
+              <NavLink to="/" className="pb-1" onClick={() => setOpen(!open)}>
+                Home
+              </NavLink>
             </li>
             <li className="lg:px-5 py-2 block text-center hover:text-yellow-700 font-semibold">
-              <NavLink to="/about" className="pb-1">About Us</NavLink>
+              <NavLink
+                to="/about"
+                className="pb-1"
+                onClick={() => setOpen(!open)}
+              >
+                About Us
+              </NavLink>
             </li>
             <li className="lg:px-5 py-2 block text-center hover:text-yellow-700 font-semibold">
-              <NavLink to="/glossary" className="pb-1">Glossary</NavLink>
-              
+              <NavLink
+                to="/glossary"
+                className="pb-1"
+                onClick={() => setOpen(!open)}
+              >
+                Glossary
+              </NavLink>
             </li>
           </ul>
         </nav>
